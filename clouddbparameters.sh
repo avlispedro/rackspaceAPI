3fa7c14df7798f591f19a6e58d2644ee4d9cc135
avlispedro <avlispedro@hotmail.com>
2014-12-20 18:47:54 +0000

    cloud database parameters

--- clouddbparameters.sh
+++ clouddbparameters.sh
@@ -37,7 +37,7 @@ configurationfunction(){
                         read -p "What will be the Description: " description
                         read -p "What will be the configuration name:  " configname
 			allvalues="${array[@]}"
-                        curl --ciphers RC4-SHA:RC4-MD5 -X POST -H "X-Auth-Token: $token" -d "{\"configuration\":{\"datastore\":{\"type\":\"$databasetype\",\"version\":\"$databaseversion\"},\"description\":\"$description\",\"name\":\"$configname\",\"values\":{$allvalues}}}" "https://$dc.databases.api.rackspacecloud.com/v1.0/$ddi/configurations" -H "Content-Type: application/json" | python -m json.tool
+                        curl -s --ciphers RC4-SHA:RC4-MD5 -X POST -H "X-Auth-Token: $token" -d "{\"configuration\":{\"datastore\":{\"type\":\"$databasetype\",\"version\":\"$databaseversion\"},\"description\":\"$description\",\"name\":\"$configname\",\"values\":{$allvalues}}}" "https://$dc.databases.api.rackspacecloud.com/v1.0/$ddi/configurations" -H "Content-Type: application/json" | python -m json.tool
 
 #echo "DEBUG"
 #echo ${array[@]}
@@ -86,9 +86,12 @@ while true; do
 	echo "2 - List the cloud DataBase parameters"
 	echo "3 - Create a configuration"
 	echo "4 - Apply a Configurations"
-	echo "5 - Unload a configuration"
+	echo "5 - List the datastores"
 	echo "6 - Delete configuration"
-	echo "7 - EXIT" 
+	echo "7 - Check which Database the configuration was applied"
+	echo "8 - List a configuration details"
+	echo "9 - Unload a Configurations from a DB"
+	echo "10 - EXIT" 
 	read  response
 	
 	if [ $response = 1 ]; then
@@ -127,13 +130,16 @@ while true; do
                                 echo "Percona NOT AVAILABLE YET...."
                                 echo "-----------------------------"
                         elif [ $databasetype = mysql ]; then 
-                                databasetype="10000000-0000-0000-0000-000000000001"
+                                #databasetype="10000000-0000-0000-0000-000000000001"
+				 databasetype="mysql"
                                 read -p "DataBase Version, 5.1 or 5.6   :" databaseversion                      
                                         if [ $databaseversion = "5.1" ]; then
-                                                databaseversion="20000000-0000-0000-0000-000000000002"
+                                                #databaseversion="20000000-0000-0000-0000-000000000002"
+						 databaseversion="5.1"
 						configurationfunction databasetype databaseversion dc ddi
                                         elif [ $databaseversion = "5.6" ]; then
-                                                databaseversion="b34a2076-c426-4ce0-8775-52b666c06c4d"
+                                                #databaseversion="b34a2076-c426-4ce0-8775-52b666c06c4d"
+						 databaseversion="5.6"
 						configurationfunction databasetype databaseversion dc ddi
                                         else
                                                 echo "Please select one of the availavle optiobs."
@@ -142,9 +148,27 @@ while true; do
                         else
                                 break
                         fi
+	elif [ $response = 5 ]; then
+		curl -s --ciphers RC4-SHA:RC4-MD5 -H 'X-Auth-Token: '$token'' 'https://'$dc'.databases.api.rackspacecloud.com/v1.0/'$ddi'/datastores' -H 'Content-Type: application/json' | python -m json.tool
+	elif [ $response = 4 ]; then
+		read -p "What is the configuration ID that you want to apply:  " confID
+		curl -s --ciphers RC4-SHA:RC4-MD5 -H 'X-Auth-Token: '$token'' 'https://'$dc'.databases.api.rackspacecloud.com/v1.0/'$ddi'/instances' -H 'Content-Type: application/json' | python -m json.tool
+		read -p "What is the DATABASE ID that you want to use:  " databaseID
+		curl --ciphers RC4-SHA:RC4-MD5 -i -X PUT -H "X-Auth-Token: $token" -H "Content-Type: application/json" https://$dc.databases.api.rackspacecloud.com/v1.0/$ddi/instances/$databaseID -d "{\"instance\":{\"configuration\":\"$confID\"}}"
 	elif [ $response = 6 ]; then
 		read -p "What is the configuration ID that you want to delete:  " deleteconfig	
 		curl -s -i  --ciphers RC4-SHA:RC4-MD5 -X DELETE -H 'X-Auth-Token: '$token'' "https://"$dc".databases.api.rackspacecloud.com/v1.0/"$ddi"/configurations/"$deleteconfig""
+	elif [ $response = 7 ]; then
+		read -p "What is the configuration ID that you want to check:  " checkconfig	
+		curl -s --ciphers RC4-SHA:RC4-MD5 -H "X-Auth-Token: $token" https://$dc.databases.api.rackspacecloud.com/v1.0/$ddi/configurations/$checkconfig/instances -H "Content-Type: application/json" | python -m json.tool
+	elif [ $response = 8 ]; then
+		read -p "What is the configuration ID that you want to check:  " checkconfig  
+		curl -s --ciphers RC4-SHA:RC4-MD5 -H "X-Auth-Token: $token" https://$dc.databases.api.rackspacecloud.com/v1.0/$ddi/configurations/$checkconfig -H "Content-Type: application/json" | python -m json.tool
+	elif [ $response = 9 ]; then
+                read -p "What is the configuration ID that you want to USE:  " confID
+                curl -s --ciphers RC4-SHA:RC4-MD5 -H 'X-Auth-Token: '$token'' 'https://'$dc'.databases.api.rackspacecloud.com/v1.0/'$ddi'/instances' -H 'Content-Type: application/json' | python -m json.tool
+                read -p "What is the DATABASE ID that you want to use:  " databaseID
+                curl --ciphers RC4-SHA:RC4-MD5 -i -X DELETE -H "X-Auth-Token: $token" -H "Content-Type: application/json" https://$dc.databases.api.rackspacecloud.com/v1.0/$ddi/instances/$databaseID -d "{\"instance\":{\"configuration\":\"$confID\"}}"
 	else
 		break
 	fi
@@ -152,14 +176,9 @@ done
 
 #delete temporary file used to auth
 auth="/tmp/auth.temp"
-dbfile="/tmp/db.temp"
+#dbfile="/tmp/db.temp"
 if [ -f $auth ]; then
 	rm $auth
-else
-	echo ""
-fi
-if [ -f $dbfile ]; then
-	rm $dbfile
 else
 	echo ""
 fi
